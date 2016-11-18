d42762ca9167f988b3ff962228b9f78f0d03544b
James Ahlm <14782828428@qq.com>
2016-11-18 22:20:09 +0800

    Added topojson file

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,25 +1,29 @@
-var githubUrl = 'https://github.com/jtedwards21/mapdata/blob/master/public/json/ne_50m_admin_0_countries.geojson'
+var githubUrl = "https://raw.githubusercontent.com/jtedwards21/mapdata/master/public/json/ne_50m_admin_0_countries.geojson"
 
 var width = 900
 var height = 600
 
-var projection = d3.geoConicEqualArea()
+var projection = d3.geo.conicEqualArea()
 
 var svg = d3.select("#chart")
 
 svg.attr("width", width)
 .attr("height", height)
 
-var path = d3.geoPath()
+var path = d3.geo.path()
 .projection(projection)
 
 var g = svg.append("g");
 
 
 //How can I get JSON off github?
-d3.json("world.json", function(error, topology){
+d3.json(githubUrl, function(error, topology){
+window.topology = topology
+console.log('l')
+var t = topojson.object(topology.features)
+console.log(t)
 g.selectAll("path")
-.data(topojson.object(topology, topology.objects.countries).geometries)
+.data(t.geometries)
 .enter()
 .append("path")
 .attr("d", path)
