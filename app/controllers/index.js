8f6080e75788784f4af9b3b26269a7b204e06e97
James Ahlm <14782828428@qq.com>
2016-12-23 23:37:56 +0800

    Meteors move with countries

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -2,79 +2,52 @@ h = innerHeight;
 w = innerWidth;
 
 
-var makeMap = function(url){
+var makeMap = function(){
 //Use own server to serve map content
 
   //Set up SVG
   var svg = d3.select("#chart")
+  svg.attr("width", innerWidth)
+  .attr("height", innerHeight);
 
-
+  //Set up Projection
   var projection = d3.geo.mercator();
+  s = 300;
+  projection.scale(s)
+
+  var t = [innerWidth / 2,innerHeight /2];
+  projection.translate(t);
+
+  var map = svg.append('g').attr('class', 'boundary');
+  d3.selectAll(".boundary")
+    .attr("width", innerWidth)
+    .attr("height", innerHeight);
 
   var path = d3.geo.path()
     .projection(projection);
 　　
-  d3.json(url, function(error, json){
+  //Get Meteors
+  var meteors = svg.append('g')
 
-    var countries = topojson.feature(json, json.objects.ne_50m_admin_0_countries)
-    var b = path.bounds(countries);
+  var l;
+  var m;
 
+　//Get Json
+ d3.json("/public/data/map.json", function(error, json){
+    var countries = topojson.feature(json, json.objects.ne_50m_admin_0_countries);
 
-    svg.attr("width", innerWidth)
-    .attr("height", innerHeight);
-    s = 300;
-    projection.scale(s)
-
-    d3.selectAll(".boundary")
-    .attr("width", innerWidth)
-    .attr("height", innerHeight);
-
-    var t = [innerWidth / 2,innerHeight /2];
-    projection.translate(t);
-
-    var dragging = function(){
-      var oldT = projection.translate();
-    };
-    var flag = [0,0]
-    
-    
-
-    var map = svg.append('g').attr('class', 'boundary');
-    
-
-
-    var l  = map.selectAll('path').data(countries.features)
-　　　　l.enter()
+    l = map.selectAll('path').data(countries.features)
+　　　　.enter()
     .append('path')
     .attr('d', path)
     .attr('fill', 'red');
 
-    svg
-    .on("mousedown", function(){
-	flag = [d3.event.clientX,d3.event.clientY];
-}, false)
-    .on("mouseup", function(){
-	var newT = [d3.event.clientX - flag[0], d3.event.clientY - flag[1]];
-	oldT = projection.translate();
-	projection.translate([newT[0]+oldT[0], newT[1]+oldT[1]]);
-        l.attr('d', path);
-}, false);
-
+    var flag = [0,0]
 
   });
 
-  console.log(projection.translate());
-  
-  drawMeteors("/public/data/meteors.json", projection);
-
-};
-
-var drawMeteors = function(url, projection){
-  svg = d3.select("#chart");
-  d3.json(url, function(json){
-     var meteors = svg.append('g')
-     .selectAll('path')
-     .data(json.features)
+  d3.json("/public/data/meteors.json", function(json){
+      m = meteors.selectAll('path').data(json.features)
      .enter()
      .append('circle')
      .attr('cx', function(d){
@@ -84,10 +57,31 @@ var drawMeteors = function(url, projection){
         return projection([d.properties.reclong,d.properties.reclat])[1] 
       })
      .attr('r', 2)
-     .attr('fill', 'blue')
-     
+     .attr('fill', 'blue');
 
-     })
-}
+    var flag = [0,0]
 
-makeMap("/public/data/map.json");
+    svg
+    .on("mousedown", function(){
+	flag = [d3.event.clientX,d3.event.clientY];
+}, false)
+    .on("mouseup", function(){
+	var newT = [d3.event.clientX - flag[0], d3.event.clientY - flag[1]];
+	oldT = projection.translate();
+	projection.translate([newT[0]+oldT[0], newT[1]+oldT[1]]);
+	l.attr('d', path);
+        
+         m.attr('cx', function(d){
+	return projection([d.properties.reclong, d.properties.reclat])[0] 
+      })
+     m.attr('cy', function(d) { 
+        return projection([d.properties.reclong,d.properties.reclat])[1] 
+      })
+}, false);
+   });
+
+
+};
+
+
+makeMap();
